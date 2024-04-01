98f458ec0dc37484d12ba71d8ee05927db12e8f7
Milan-Uy <uymillano@gmail.com>
2024-04-01 19:00:14 +0800

    proper error handling & finishing auth

--- src/lib/validators/account-credentials-validator.ts
+++ src/lib/validators/account-credentials-validator.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-
+// change 3 to 8 min char
 export const AuthCredentialsValidator = z.object({
   email: z.string().email(),
   password: z.string().min(3, { message: 'Password must be at least 8 characters long.' }),
